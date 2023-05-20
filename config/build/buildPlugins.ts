3cb06015ede17d0be9173597c4f35db282eec452
Vasya <chatterbox1995@mail.ru>
2023-05-20 17:39:05 +0300

    make webpack bundle analyzer and hot module replacement isdev flag

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,22 +3,30 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import plugin from 'babel-plugin-i18next-extract';
 
 export const buildPlugins = ({
   paths,
   isDev,
-}: Pick<BuildOptions, 'paths' | 'isDev'>): webpack.WebpackPluginInstance[] => [
-  new webpack.ProgressPlugin(),
-  new HTMLWebpackPlugin({
-    template: paths.html,
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({ openAnalyzer: false }),
-];
+}: Pick<BuildOptions, 'paths' | 'isDev'>): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+
+  return plugins;
+};
