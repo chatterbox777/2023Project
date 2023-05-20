ce5bd0fca54e3ed0d3fc0d0da0257ee030f7b6d7
Vasya <chatterbox1995@mail.ru>
2023-05-20 17:33:23 +0300

    fix ts error

--- config/build/loaders/buildCssLoaders.ts
+++ config/build/loaders/buildCssLoaders.ts
@@ -1,6 +1,6 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-export const buildCssLoader = (isDev) => ({
+export const buildCssLoader = (isDev: boolean) => ({
   test: /\.s[ac]ss$/i,
   use: [
     isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
